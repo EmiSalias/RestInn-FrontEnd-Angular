f833848fe51c6487fe9fe477c85dce7f2e2908f0
EmiSalias <emiliano.salias99@gmail.com>
2025-10-26 16:26:02 -0300

    Submenu en el sidebar, agregado de fonts icons

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
