03f46b6a73ad313844f8b27ea6995dc7f66bd4cd
EmiSalias <emiliano.salias99@gmail.com>
2025-11-16 17:57:34 -0300

    Se trabaja con los componentes accesibles desde el footer

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule,FontAwesomeModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
 
+  private fb = inject(FormBuilder);
+
+  // Iconos
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMapMarkerAlt = faMapMarkerAlt;
+
+  // Estados del formulario
+  loading = false;
+  submitted = false;
+
+  // EL FORMULARIO NO SE ENCUENTRA EN FUNCIONAMIENTO PORQUE NO LO IMPLEMENTAMOS EN EL BACK
+  contactForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    mensaje: ['', [Validators.required, Validators.minLength(10)]]
+  });
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    
+    setTimeout(() => {
+      this.loading = false;
+      this.submitted = true;
+    }, 1500);
+  }
 }
